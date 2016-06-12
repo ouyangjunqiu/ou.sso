968d60a9771e4e05c037ab752ff5361e85d74d84
oShine <oyjqdlp@126.com>
2016-06-12 14:30:18 +0800

    Update do.php
    屏蔽错误提示

--- server/do.php
+++ server/do.php
@@ -7,17 +7,17 @@ function LoginValidate($username,$password,&$user){
     return $username == $password;
 }
 
-ini_set('session.gc_maxlifetime', 30*24*3600);
-ini_set('session.use_cookies',1);
+@ini_set('session.gc_maxlifetime', 30*24*3600);
+@ini_set('session.use_cookies',1);
 
-session_start();
+@session_start();
 
 if(empty($_SESSION["ref"]))
     $_SESSION["ref"] = "user.php";
 $postData = $_REQUEST["LoginForm"];   
 if(!empty($postData) && LoginValidate($postData["username"],$postData["password"],$loginUser)){
     $_SESSION["user"] = $loginUser;
-    setcookie("loginToken",$loginUser["id"],$expire,"/",".".$_SERVER['HTTP_HOST']);
+    @setcookie("loginToken",$loginUser["id"],$expire,"/",".".$_SERVER['HTTP_HOST']);
     header("Location:".$_SESSION["ref"]);
 }else{
     header("Location:index.php?ref=".$_SESSION["ref"]."&retry=true");
