b9442b2ce03fe6022e32ef405d10678c7c4fc33d
oShine <oyjqdlp@126.com>
2016-06-12 14:27:48 +0800

    Update user.php

--- server/user.php
+++ server/user.php
@@ -6,10 +6,10 @@ function GetUserByToken($token){
     return $token;
 }
 
-ini_set('session.gc_maxlifetime', 30*24*3600);
-ini_set('session.use_cookies',1);
+@ini_set('session.gc_maxlifetime', 30*24*3600);
+@ini_set('session.use_cookies',1);
 
-session_start();
+@session_start();
 
 $callback = $_REQUEST["callbackparam"];
 if(isset($_SESSION["user"]) && !empty($_SESSION["user"])){
@@ -17,8 +17,7 @@ if(isset($_SESSION["user"]) && !empty($_SESSION["user"])){
 }else if(isset($_COOKIE["loginToken"])){
     $res = GetUserByToken($_COOKIE["loginToken"]);
     if(!empty($res)){
-        $res = json_decode(json_encode($res),true);
-        $_SESSION["user"] = array_merge($res,array("username"=>$res["name"]));
+        $_SESSION["user"] = json_decode(json_encode($res),true);
         echo $callback."(".json_encode(array("islogin"=>true,'data'=>$_SESSION["user"])).")";
     }else{
         echo $callback."(".json_encode(array("islogin"=>false)).")";
